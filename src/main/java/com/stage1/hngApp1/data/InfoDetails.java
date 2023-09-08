249fc161897d8a0c353a7d5126858bd041e61aa9
chinwoke <canugwara99@gmail.com>
2023-09-08 09:28:43 +0100

    models created

--- src/main/java/com/stage1/hngApp1/data/InfoDetails.java
+++ src/main/java/com/stage1/hngApp1/data/InfoDetails.java
@@ -1,4 +1,16 @@
 package com.stage1.hngApp1.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class InfoDetails {
+    private String slackName;
+    private String currentDayOfTheWeek;
+    private String currentUTCTime;
+    private String track;
+    private String githubFileUrl;
+    private String githubRepoUrl;
+    private final int status = 200;
 }
